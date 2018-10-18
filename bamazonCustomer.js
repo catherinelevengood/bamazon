35cf5c2c7b37c334e38f957fc59f44ccf18d2a97
Catherine Levengood <angel_avelanes@yahoo.com>
2018-10-17 21:31:48 -0400

    create gitignore installed npm mysql,inquirer and started node file create database connection

---
+++ bamazonCustomer.js
@@ -0,0 +1,21 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 8889,
+
+    user: "root",
+
+    password: "",
+    database: "bamazon_DB"
+
+});
+
+connection.connect(function(err){
+    if(err)throw err;
+    console.log("hello");
+    connection.end()
+}
+)
